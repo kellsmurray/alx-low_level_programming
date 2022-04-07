e632594b8f10b43cec1e0c00250e52b71c99cafc
kells <mchungajy@gmail.com>
2022-04-07 10:40:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
- * 
+ * main - Entry point 
+ * Description: prints "with proper grammar, but the outcome is a
+ * piece of art,"
  * Return: Always 0 (Success)
  */
 int main(void)

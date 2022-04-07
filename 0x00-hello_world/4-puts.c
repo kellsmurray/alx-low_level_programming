f9b9ff22a2ac22d59e5505679b3e20d0cd0de008
kells <mchungajy@gmail.com>
2022-04-07 09:02:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 /**
  * main - the first entry point to the program
  *
- * puts - displays content on output string
+ * Description - displays content on output string
  *
- * return 0 - correct output when code runs successfully
- *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+  	puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }

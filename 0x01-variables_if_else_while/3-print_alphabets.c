4619f7cbce6df1b19150a305d627d68ebc6ff573
kells <mchungajy@gmail.com>
2022-04-08 14:55:49 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 97;
+int j = 65;
+
+while (i < 123)
+{
+putchar(i);
+i++;
+}
+ 
+while (j < 91)
+{
+putchar(j);
+j++;
+}
+ 
+putchar(10);
+
+return (0);
+}

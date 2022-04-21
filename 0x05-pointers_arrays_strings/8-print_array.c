2bf3b1be3fc50133f4416d19eb94793600e7da97
kells <mchungajy@gmail.com>
2022-04-21 13:23:54 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,22 +10,14 @@
 
 void print_array(int *a, int n)
 {
-int i, count;
+short c = 0;
 
-i = 0;
-count = 1;
-if (n < 0)
-n = 0;
-if (n > 0)
+while (n-- > 0)
 {
-while (a[i] != '\0' && count < n)
-{
-printf("%d, ", a[i]);
-i++;
-count++;
+printf("%d", a[c++]);
+if (n != 0)
+printf(", ");
 }
 
 printf("\n");
-
-}
 }

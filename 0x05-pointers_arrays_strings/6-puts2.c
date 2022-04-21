f46b7ab9e05d76fc5126c28514213d4c71c32b80
kells <mchungajy@gmail.com>
2022-04-20 21:41:37 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,10 @@ void puts2(char *str)
 
 int i = 0;
 
-for (str[i] = 0; str[i] != '\0'; i += 2)
+for (str[i] = 0; str[i] != '\0'; i++)
 {
+if (i % 2 == 0)
 _putchar(str[i]);
 }
- _putchar('\n');
+_putchar('\n');
 }

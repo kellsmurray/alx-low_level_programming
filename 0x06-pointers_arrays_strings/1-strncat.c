0e787b1f162e227aebe1e75a1ae794a31ca90072
kells <mchungajy@gmail.com>
2022-04-25 01:38:12 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - Concantenate two strings using @ bytes
+ *
+ * @dest: Destination string; src string appended to it
+ * @src: Source string; gets appended to destination string
+ * @n: Number of bytes of src that should be concantenated with dest
+ *
+ * Return: Pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+int i  = 0;
+int j= 0;
+
+while (dest[i] != '\0')
+i++;
+
+while (src[j] != '\0' && n != j)
+{
+*(dest + 1) = src[j];
+j++;
+i++;
+}
+
+*(dest + 1) = '\0';
+
+return (dest);
+}

d5377e44ba30eb8575558d8ab2e2c3ec44026384
kells <mchungajy@gmail.com>
2022-05-03 06:59:10 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -22,7 +22,7 @@ return (0);
 
 else
 {
-printf("Error");                                                                                                                          
+printf("Error\n");                                                                                                                          
 return (1);
 }
 

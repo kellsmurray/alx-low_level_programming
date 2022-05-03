006122f8afcb9a2a210cb67798ec6317e6038b8a
kells <mchungajy@gmail.com>
2022-05-03 07:49:59 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,85 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * change - finds least number of coins for change
+ * @cash: money amount
+ * Return: number of coins
+ */
+
+int make_change(int cash)
+{
+
+int total_coins = 0, twenty_fives = 0, tens = 0, fives = 0, twos = 0, ones = 0;
+
+while (cash > 0)
+{
+
+if (cash >= 25)
+{
+cash = cash - 25;
+twenty_fives = twenty_fives + 1;
+}
+
+else if (cash >= 10)
+{
+cash = cash - 10;
+tens = tens + 1;
+}
+
+else if (cash >= 5)
+{
+cash = cash - 5;
+fives = fives + 1;
+}
+
+else if (cash >= 2)
+{
+cash = cash - 2;
+twos = twos + 1;
+}
+
+else
+{
+cash = cash - 1;
+ones = ones + 1;
+}
+
+}
+
+coins = twenty_fives + tens + fives + twos + ones;
+return (coins);
+}
+
+/**
+ * main - prints minimum number of coins to get change
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+
+int coins;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+else if (atoi(argv[1]) < 0)
+{
+printf("0\n");
+}
+
+else
+{
+coins = total_coins(atoi(argv[1]));
+printf("%d\n", coins);
+}
+
+return (0);
+}

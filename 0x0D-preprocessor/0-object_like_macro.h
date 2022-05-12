c950c42f3823abd70e901cce0fb8c9aa43e6b84f
kells <mchungajy@gmail.com>
2022-05-12 09:08:46 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef 0-OBJECT_LIKE_MACRO_H
-#define 0-OBJECT_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 /**
  * File: 0-object_like_macro.h
@@ -9,5 +9,4 @@
  */
 
 #define SIZE 1024
-
 #endif

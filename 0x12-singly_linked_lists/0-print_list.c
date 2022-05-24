c5d6e2f0276042e636e32a8cb75341f2714bbe3b
kells <you@example.com>
2022-05-24 02:47:51 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,18 +11,22 @@
 
 size_t print_list(const list_t *h)
 {
-
 size_t size;
 size = 0;
 
 while (h != NULL)
 {
 size = size + 1;
+
 if (h->str == NULL)
 printf("[%d] nil\n", 0);
+
 else
 printf("[%d] %s\n", h->len, h->str);
+
 h = h->next;
 }
+
 return (size);
+
 }

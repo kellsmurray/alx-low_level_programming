f71bd76f2b78d8ae2f60ae888518ca9eeb83f7c0
kells <you@example.com>
2022-05-24 02:44:28 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -11,13 +8,6 @@
  *
  * Description: singly linked list node structure
  */
-
-typedef struct list_s
-{
-char *str;
-unsigned int len;
-struct list_s *next;
-} list_t;
 
 size_t print_list(const list_t *h)
 {

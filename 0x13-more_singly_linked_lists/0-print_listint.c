782d2806d6db3ef28d9f41d4400f287c2a2a582e
kells <you@example.com>
2022-05-30 05:04:31 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
+ * print_listint - prints all the elements
  * @h: head of linked list
  * Return: number of nodes
  */
